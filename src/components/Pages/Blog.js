67caa82aba8f7b9e5984744ffb49c14a3568591c
Khalidprithy <khalidprithy@gmail.com>
2022-09-27 19:16:29 +0600

    blog added

--- src/components/Pages/Blog.js
+++ src/components/Pages/Blog.js
@@ -1,9 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { BsFillArrowRightCircleFill } from 'react-icons/bs';
 
 const Blog = () => {
+
+    const [openFront, setOpenFront] = useState(false);
+
     return (
-        <div className='flex items-center justify-center'>
-            <h1 className='text-4xl font-semibold'>Coming soon</h1>
+        <div>
+            <div className='flex items-center justify-center m-4 mt-6'>
+                <div
+                    onMouseEnter={() => setOpenFront(true)}
+                    onMouseLeave={() => setOpenFront(false)}
+                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 h-24 rounded-xl ease-in-out duration-500 mb-4'
+                >
+                    <h1 className='text-3xl text-center text-white px-4 ease-in-out duration-1000'>50 JavaScript interview Q&A</h1>
+                    <div className={` ${openFront ? "hidden md:flex justify-between items-center gap-4 ease-in-out duration-500" : "flex md:hidden"} `}>
+                        <p className='text-2xl font-bold'>View</p>
+                        <BsFillArrowRightCircleFill className='text-5xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
+                    </div>
+                </div>
+            </div>
+            <div className='flex items-center justify-center m-4'>
+
+                <div
+                    onMouseEnter={() => setOpenFront(true)}
+                    onMouseLeave={() => setOpenFront(false)}
+                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 h-24 rounded-xl ease-in-out duration-500 mb-4'
+                >
+                    <h1 className='text-3xl text-center text-white px-4 ease-in-out duration-1000'>40 CSS interview Q&A</h1>
+                    <div className={` ${openFront ? "hidden md:flex justify-between items-center gap-4 ease-in-out duration-500" : "flex md:hidden"} `}>
+                        <p className='text-2xl font-bold'>View</p>
+                        <BsFillArrowRightCircleFill className='text-5xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
+                    </div>
+                </div>
+            </div>
+            <div className='flex items-center justify-center m-4'>
+                <div
+                    onMouseEnter={() => setOpenFront(true)}
+                    onMouseLeave={() => setOpenFront(false)}
+                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 h-24 rounded-xl ease-in-out duration-500 mb-4'
+                >
+                    <h1 className='text-3xl text-center text-white px-4 ease-in-out duration-1000'>40 React interview Q&A</h1>
+                    <div className={` ${openFront ? "hidden md:flex justify-between items-center gap-4 ease-in-out duration-500" : "flex md:hidden"} `}>
+                        <p className='text-2xl font-bold'>View</p>
+                        <BsFillArrowRightCircleFill className='text-5xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
+                    </div>
+                </div>
+            </div>
+            <div className='flex items-center justify-center m-4'>
+                <div
+                    onMouseEnter={() => setOpenFront(true)}
+                    onMouseLeave={() => setOpenFront(false)}
+                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 h-24 rounded-xl ease-in-out duration-500 mb-4'
+                >
+                    <h1 className='text-3xl text-center text-white px-4 ease-in-out duration-1000'>Why waking up early is the best habit to start with.</h1>
+                    <div className={` ${openFront ? "hidden md:flex justify-between items-center gap-4 ease-in-out duration-500" : "flex md:hidden"} `}>
+                        <p className='text-2xl font-bold'>View</p>
+                        <BsFillArrowRightCircleFill className='text-5xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
+                    </div>
+                </div>
+            </div>
+            <div className='flex items-center justify-center m-4'>
+                <div
+                    onMouseEnter={() => setOpenFront(true)}
+                    onMouseLeave={() => setOpenFront(false)}
+                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 h-24 rounded-xl ease-in-out duration-500 mb-4'
+                >
+                    <h1 className='text-3xl text-center text-white px-4 ease-in-out duration-1000'>10 benefits of working out</h1>
+                    <div className={` ${openFront ? "hidden md:flex justify-between items-center gap-4 ease-in-out duration-500" : "flex md:hidden"} `}>
+                        <p className='text-2xl font-bold'>View</p>
+                        <BsFillArrowRightCircleFill className='text-5xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
