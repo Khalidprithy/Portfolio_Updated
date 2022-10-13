f0cc1b6d76a6ada6112085565dbec0a0d0758324
Khalidprithy <khalidprithy@gmail.com>
2022-10-14 02:50:18 +0600

    frontend qa added

--- src/components/Pages/Projects.js
+++ src/components/Pages/Projects.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { BsFillArrowRightCircleFill } from 'react-icons/bs';
 import { AiFillGithub } from 'react-icons/ai';
 import { CgWebsite } from 'react-icons/cg';
