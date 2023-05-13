0a850982e6aa3acea59681b4c35930ce30637536
Khalidprithy <khalidprithy@gmail.com>
2023-05-13 11:17:21 +0600

    feat- blog updated

--- src/components/Pages/Blog.js
+++ src/components/Pages/Blog.js
@@ -52,14 +52,14 @@ const Blog = () => {
                 <div
                     onMouseEnter={() => setOpenBlog0(true)}
                     onMouseLeave={() => setOpenBlog0(false)}
-                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-24 rounded-xl ease-in-out duration-500 mb-4'
+                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-16 rounded-xl ease-in-out duration-500 mb-4'
                 >
-                    <h4 className='text-xl md:text-3xl text-center text-white px-4 ease-in-out duration-1000'>Daily Blog of MERN</h4>
+                    <h4 className='text-xl md:text-2xl text-center text-white px-4 ease-in-out duration-1000'>Daily Blog of MERN</h4>
                     <div className={` ${openBlog0 ? "flex justify-between items-center gap-4 ease-in-out duration-500" : "flex items-center gap-1 md:hidden"} `}>
-                        <p className='text-xl md:text-2xl font-bold'>View</p>
+                        <p className='text-xl md:text-2xl font-semibold'>View</p>
                         <BsFillArrowRightCircleFill
                             onClick={handleDailyBlog}
-                            className='text-xl md:text-5xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
+                            className='text-xl md:text-4xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
                     </div>
                 </div>
             </div>
@@ -67,14 +67,14 @@ const Blog = () => {
                 <div
                     onMouseEnter={() => setOpenBlog0(true)}
                     onMouseLeave={() => setOpenBlog0(false)}
-                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-24 rounded-xl ease-in-out duration-500 mb-4'
+                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-16 rounded-xl ease-in-out duration-500 mb-4'
                 >
-                    <h4 className='text-xl md:text-3xl text-center text-white px-4 ease-in-out duration-1000'>25 HTML interview Q&A</h4>
+                    <h4 className='text-xl md:text-2xl text-center text-white px-4 ease-in-out duration-1000'>25 HTML interview Q&A</h4>
                     <div className={` ${openBlog0 ? "flex justify-between items-center gap-4 ease-in-out duration-500" : "flex items-center gap-1 md:hidden"} `}>
-                        <p className='text-xl md:text-2xl font-bold'>View</p>
+                        <p className='text-xl md:text-2xl font-semibold'>View</p>
                         <BsFillArrowRightCircleFill
                             onClick={handleHTMLQa}
-                            className='text-xl md:text-5xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
+                            className='text-xl md:text-4xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
                     </div>
                 </div>
             </div>
@@ -82,14 +82,14 @@ const Blog = () => {
                 <div
                     onMouseEnter={() => setOpenBlog7(true)}
                     onMouseLeave={() => setOpenBlog7(false)}
-                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-24 rounded-xl ease-in-out duration-500 mb-4'
+                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-16 rounded-xl ease-in-out duration-500 mb-4'
                 >
-                    <h4 className='text-xl md:text-3xl text-center text-white px-4 ease-in-out duration-1000'>25 CSS interview Q&A</h4>
+                    <h4 className='text-xl md:text-2xl text-center text-white px-4 ease-in-out duration-1000'>25 CSS interview Q&A</h4>
                     <div className={` ${openBlog7 ? "flex justify-between items-center gap-4 ease-in-out duration-500" : "flex items-center gap-1 md:hidden"} `}>
-                        <p className='text-xl md:text-2xl font-bold'>View</p>
+                        <p className='text-xl md:text-2xl font-semibold'>View</p>
                         <BsFillArrowRightCircleFill
                             onClick={handleCSSQa}
-                            className='text-xl md:text-5xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
+                            className='text-xl md:text-4xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
                     </div>
                 </div>
             </div>
@@ -97,14 +97,14 @@ const Blog = () => {
                 <div
                     onMouseEnter={() => setOpenBlog1(true)}
                     onMouseLeave={() => setOpenBlog1(false)}
-                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-24 rounded-xl ease-in-out duration-500 mb-4'
+                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-16 rounded-xl ease-in-out duration-500 mb-4'
                 >
-                    <h4 className='text-xl md:text-3xl text-center text-white px-4 ease-in-out duration-1000'>40 JavaScript interview Q&A</h4>
+                    <h4 className='text-xl md:text-2xl text-center text-white px-4 ease-in-out duration-1000'>40 JavaScript interview Q&A</h4>
                     <div className={` ${openBlog1 ? "flex justify-between items-center gap-4 ease-in-out duration-500" : "flex items-center gap-1 md:hidden"} `}>
-                        <p className='text-xl md:text-2xl font-bold'>View</p>
+                        <p className='text-xl md:text-2xl font-semibold'>View</p>
                         <BsFillArrowRightCircleFill
                             onClick={handleJavaScriptQa}
-                            className='text-xl md:text-5xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
+                            className='text-xl md:text-4xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
                     </div>
                 </div>
             </div>
@@ -113,14 +113,14 @@ const Blog = () => {
                 <div
                     onMouseEnter={() => setOpenBlog2(true)}
                     onMouseLeave={() => setOpenBlog2(false)}
-                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-24 rounded-xl ease-in-out duration-500 mb-4'
+                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-16 rounded-xl ease-in-out duration-500 mb-4'
                 >
-                    <h4 className='text-xl md:text-3xl text-center text-white px-4 ease-in-out duration-1000'>40 NodeJS interview Q&A</h4>
+                    <h4 className='text-xl md:text-2xl text-center text-white px-4 ease-in-out duration-1000'>40 NodeJS interview Q&A</h4>
                     <div className={` ${openBlog2 ? "flex justify-between items-center gap-4 ease-in-out duration-500" : "flex items-center gap-1 md:hidden"} `}>
-                        <p className='text-xl md:text-2xl font-bold'>View</p>
+                        <p className='text-xl md:text-2xl font-semibold'>View</p>
                         <BsFillArrowRightCircleFill
                             onClick={handleNodeQa}
-                            className='text-xl md:text-5xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
+                            className='text-xl md:text-4xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
                     </div>
                 </div>
             </div>
@@ -128,14 +128,14 @@ const Blog = () => {
                 <div
                     onMouseEnter={() => setOpenBlog3(true)}
                     onMouseLeave={() => setOpenBlog3(false)}
-                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-24 rounded-xl ease-in-out duration-500 mb-4'
+                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-16 rounded-xl ease-in-out duration-500 mb-4'
                 >
-                    <h4 className='text-xl md:text-3xl text-center text-white px-4 ease-in-out duration-1000'>40 React interview Q&A</h4>
+                    <h4 className='text-xl md:text-2xl text-center text-white px-4 ease-in-out duration-1000'>40 React interview Q&A</h4>
                     <div className={` ${openBlog3 ? "flex justify-between items-center gap-4 ease-in-out duration-500" : "flex items-center gap-1 md:hidden"} `}>
-                        <p className='text-xl md:text-2xl font-bold'>View</p>
+                        <p className='text-xl md:text-2xl font-semibold'>View</p>
                         <BsFillArrowRightCircleFill
                             onClick={handleReactQa}
-                            className='text-xl md:text-5xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
+                            className='text-xl md:text-4xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
                     </div>
                 </div>
             </div>
@@ -143,14 +143,14 @@ const Blog = () => {
                 <div
                     onMouseEnter={() => setOpenBlog4(true)}
                     onMouseLeave={() => setOpenBlog4(false)}
-                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-24 rounded-xl ease-in-out duration-500 mb-4'
+                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-16 rounded-xl ease-in-out duration-500 mb-4'
                 >
-                    <h4 className='text-xl md:text-3xl text-center text-white px-4 ease-in-out duration-1000'>40 MongoDB interview Q&A</h4>
+                    <h4 className='text-xl md:text-2xl text-center text-white px-4 ease-in-out duration-1000'>40 MongoDB interview Q&A</h4>
                     <div className={` ${openBlog4 ? "flex justify-between items-center gap-4 ease-in-out duration-500" : "flex items-center gap-1 md:hidden"} `}>
-                        <p className='text-xl md:text-2xl font-bold'>View</p>
+                        <p className='text-xl md:text-2xl font-semibold'>View</p>
                         <BsFillArrowRightCircleFill
                             onClick={handleMongoQa}
-                            className='text-xl md:text-5xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
+                            className='text-xl md:text-4xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
                     </div>
                 </div>
             </div>
@@ -158,14 +158,14 @@ const Blog = () => {
                 <div
                     onMouseEnter={() => setOpenBlog5(true)}
                     onMouseLeave={() => setOpenBlog5(false)}
-                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-24 rounded-xl ease-in-out duration-500 mb-4'
+                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-16 rounded-xl ease-in-out duration-500 mb-4'
                 >
-                    <h4 className='text-xl md:text-3xl text-center text-white px-4 ease-in-out duration-1000'>Why waking up early is the best habit</h4>
+                    <h4 className='text-xl md:text-2xl text-center text-white px-4 ease-in-out duration-1000'>Why waking up early is the best habit</h4>
                     <div className={` ${openBlog5 ? "flex justify-between items-center gap-4 ease-in-out duration-500" : "flex items-center gap-1 md:hidden"} `}>
-                        <p className='text-xl md:text-2xl font-bold'>View</p>
+                        <p className='text-xl md:text-2xl font-semibold'>View</p>
                         <BsFillArrowRightCircleFill
                             onClick={handleWakeUp}
-                            className='text-xl md:text-5xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
+                            className='text-xl md:text-4xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
                     </div>
                 </div>
             </div>
@@ -173,14 +173,14 @@ const Blog = () => {
                 <div
                     onMouseEnter={() => setOpenBlog6(true)}
                     onMouseLeave={() => setOpenBlog6(false)}
-                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-24 rounded-xl ease-in-out duration-500 mb-4'
+                    className='flex flex-col md:flex-row items-center justify-around bg-neutral text-white w-full md:w-6/12 mx-auto hover:w-10/12 hover:text-accent h-16 rounded-xl ease-in-out duration-500 mb-4'
                 >
-                    <h4 className='text-xl md:text-3xl text-center text-white px-4 ease-in-out duration-1000'>10 benefits of working out</h4>
+                    <h4 className='text-xl md:text-2xl text-center text-white px-4 ease-in-out duration-1000'>10 benefits of working out</h4>
                     <div className={` ${openBlog6 ? "flex justify-between items-center gap-4 ease-in-out duration-500" : "flex items-center gap-1 md:hidden"} `}>
-                        <p className='text-xl md:text-2xl font-bold'>View</p>
+                        <p className='text-xl md:text-2xl font-semibold'>View</p>
                         <BsFillArrowRightCircleFill
                             onClick={handleWorkOut}
-                            className='text-xl md:text-5xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
+                            className='text-xl md:text-4xl hover:text-error ease-out duration-300'></BsFillArrowRightCircleFill>
                     </div>
                 </div>
             </div>
